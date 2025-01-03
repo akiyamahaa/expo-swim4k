7d3575546a43b3b7a96359bb4f5ee4d5bcac5d2e
tuanna-kite <tuanna.kiteacademy@gmail.com>
2025-01-03 19:51:09 +0700

    query lesson

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import { router } from 'expo-router'
 import { images } from '@/constants'
 import { ERouteTable } from '@/constants/route-table'
-import { fetchAll } from '@/services/lesson-group.service'
 
 export default function Root() {
   useEffect(() => {
@@ -11,10 +10,6 @@ export default function Root() {
       router.replace(ERouteTable.SIGIN_IN)
     }, 200)
     return () => clearTimeout(timer)
-  }, [])
-
-  useEffect(() => {
-    fetchAll()
   }, [])
 
   return (
--- src/services/lesson.service.ts
+++ src/services/lesson.service.ts
@@ -1,10 +1,41 @@
 import { supabase } from '@/lib/supabase'
 import { Lesson } from '@/models/lessons.model'
+import { createDAO, ETableName } from './dao.service'
 
 // TODO: Fetch all Lesson by Lesson Group
-export async function fetchByGroup(lessonGroupId: number) {}
+export async function fetchByGroup(lessonGroupId: number) {
+  const dao = createDAO(ETableName.LESSONS)
+  const queryResult = await dao.select<string, Lesson>('*').eq('lesson_group_id', lessonGroupId)
+
+  if (queryResult.error || !queryResult.data) {
+    return {
+      error: queryResult.error.message,
+      data: [],
+    }
+  }
+
+  console.log(JSON.stringify(queryResult.data, null, 2))
+
+  return {
+    data: queryResult.data,
+  }
+}
 
 // TODO: Fetch detail Lesson
 export async function show(id: number) {
+  const dao = createDAO(ETableName.LESSONS)
+  const queryResult = await dao.select<string, Lesson>('*').eq('id', id)
 
+  if (queryResult.error || !queryResult.data) {
+    return {
+      error: queryResult.error.message,
+      data: [],
+    }
+  }
+
+  console.log(JSON.stringify(queryResult.data, null, 2))
+
+  return {
+    data: queryResult.data,
+  }
 }
